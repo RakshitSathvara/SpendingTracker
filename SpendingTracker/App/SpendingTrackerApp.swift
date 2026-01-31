779160d9118f2fbdb5d92841f92513c2178eb563
Rakshit Sathvara <rakshit.sathwara@tmspl.com>
2026-01-31 19:48:00 +0530

    Initial project setup - iOS 26 with SwiftData and Firebase

---
+++ SpendingTracker/App/SpendingTrackerApp.swift
@@ -0,0 +1,43 @@
+//
+//  SpendingTrackerApp.swift
+//  SpendingTracker
+//
+//  Created by Rakshit on 31/01/26.
+//
+
+import SwiftUI
+import SwiftData
+import FirebaseCore
+
+@main
+struct SpendingTrackerApp: App {
+    
+    // MARK: - Firebase Initialization
+    init() {
+        FirebaseApp.configure()
+    }
+    
+    // MARK: - SwiftData Container
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            // Models will be added in RAK-74
+        ])
+        let modelConfiguration = ModelConfiguration(
+            schema: schema,
+            isStoredInMemoryOnly: false
+        )
+        
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
